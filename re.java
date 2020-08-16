17976190e15c68c1fec52e79607457836aa16a1f
vsprogrammer2909 <vindyashivam2909@gmail.com>
2020-08-16 21:33:11 +0530

    first commit

---
+++ re.java
@@ -0,0 +1,15 @@
+class Input
+{
+    public static void main(String args[])
+    {
+        System.out.println("vindya shivam");
+        int i=10;
+        if(i==20)
+        {
+            System.out.println("hi");
+        }
+        else{
+            System.out.print("youn");
+        }
+    }
+}
---
+++ yo.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>)
+{
+	val age=95;
+	// println(n);
+	if(age>100)
+	{
+		println("you dont know how to play chess at grandmaster level");
+	}
+	else
+	{
+		println("you can play");
+	}
+}
